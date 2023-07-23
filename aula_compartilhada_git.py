3b1dca58e16ccea3b4397aafd9fe817b8ffab6d3
roger-trindade <rogerv.t18@gmailm>
2023-07-23 10:06:44 -0300

    primeiro teste git do italo marques

---
+++ aula_compartilhada_git.py
@@ -0,0 +1,5 @@
+nome = str(input('Qual o seu nome?'))
+email= str(input('Qualo o seu email?'))
+senha = str(input('Qual a sua senha?'))
+
+print('Bem vindo(a) Sr(a) '+nome+'. O seu email escolhido foi: '+email+', e sua senha: '+senha+'. Aproveite a aula!')
