8a5012afc76f095f41bde8fea9b3772b9ff9ea25
ItaloMarquesGit <italomarquesgit@gmail.com>
2023-07-23 10:28:38 -0300

    estraguei tudo

--- aula_compartilhada_git.py
+++ aula_compartilhada_git.py
@@ -2,4 +2,5 @@ nome = str(input('Qual o seu nome?'))
 email= str(input('Qualo o seu email?'))
 senha = str(input('Qual a sua senha?'))
 #Teste
-print('Bem vindo(a) Sr(a) '+nome+'. O seu email escolhido foi: '+email+', e sua senha: '+senha+'. Aproveite a aula!')
+
+print('Bem vindo(a) Sr(a) '+nome+'. O seu email escolhido foi: '+email+', e sua senha: '+senha+'. Aproveite a aula!')''
