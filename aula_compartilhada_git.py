783ffc49b4cc82ea3bf393eb3fda19fa9617b343
ItaloMarquesGit <italomarquesgit@gmail.com>
2023-07-23 11:11:16 -0300

    nova atualizacao nao cagada

--- aula_compartilhada_git.py
+++
@@ -1,6 +0,0 @@
-nome = str(input('Qual o seu nome?'))
-email= str(input('Qualo o seu email?'))
-senha = str(input('Qual a sua senha?'))
-#Teste
-
-print('Bem vindo(a) Sr(a) '+nome+'. O seu email escolhido foi: '+email+', e sua senha: '+senha+'. Aproveite a aula!')''
